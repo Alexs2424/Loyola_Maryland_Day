d020049c6a0b2765970ac020bde03349bb5c8c8f
Alex <alex@Alexs-MacBook-Pro-3.local>
2018-01-17 14:04:48 -0500

    Initial Commit.

---
+++ Loyola Maryland Day/Loyola Maryland DayTests/Loyola_Maryland_DayTests.swift
@@ -0,0 +1,36 @@
+//
+//  Loyola_Maryland_DayTests.swift
+//  Loyola Maryland DayTests
+//
+//  Created by Alex on 1/17/18.
+//  Copyright © 2018 iInnovate LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Loyola_Maryland_Day
+
+class Loyola_Maryland_DayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
